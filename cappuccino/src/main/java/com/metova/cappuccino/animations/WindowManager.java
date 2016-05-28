04bb8706dca1a7bcf79465dc55eb909f62155bd7
Tony Robalik <tony.robalik@metova.com>
2016-05-28 11:43:11 -0500

    Updated WindowManager class to only get methods and object by reflection once.

--- cappuccino/src/main/java/com/metova/cappuccino/animations/WindowManager.java
+++ cappuccino/src/main/java/com/metova/cappuccino/animations/WindowManager.java
@@ -7,25 +7,37 @@ import java.lang.reflect.Method;
 
 public class WindowManager {
 
+    private Method getAnimationScales;
     private Method setAnimationScales;
     private Object windowManagerObj;
 
-    float[] getAnimationScales() throws Exception {
+    WindowManager() {
+    }
+
+    private void reflectIfNecessary() throws Exception {
+        if (getAnimationScales != null) {
+            return;
+        }
+
         Class<?> windowManagerStubClazz = Class.forName("android.view.IWindowManager$Stub");
         Method asInterface = windowManagerStubClazz.getDeclaredMethod("asInterface", IBinder.class);
         Class<?> serviceManagerClazz = Class.forName("android.os.ServiceManager");
         Method getService = serviceManagerClazz.getDeclaredMethod("getService", String.class);
         Class<?> windowManagerClazz = Class.forName("android.view.IWindowManager");
         setAnimationScales = windowManagerClazz.getDeclaredMethod("setAnimationScales", float[].class);
-        Method getAnimationScales = windowManagerClazz.getDeclaredMethod("getAnimationScales");
+        getAnimationScales = windowManagerClazz.getDeclaredMethod("getAnimationScales");
 
         IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
         windowManagerObj = asInterface.invoke(null, windowManagerBinder);
+    }
 
+    float[] getAnimationScales() throws Exception {
+        reflectIfNecessary();
         return (float[]) getAnimationScales.invoke(windowManagerObj);
     }
 
     void setAnimationScales(@NonNull float[] currentScales) throws Exception {
+        reflectIfNecessary();
         setAnimationScales.invoke(windowManagerObj, new Object[]{currentScales});
     }
 }
